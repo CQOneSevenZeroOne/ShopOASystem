fb4d7ebfece7c7eb455c69924771888c13d81242
jxf <646318689@qq.com>
2018-01-04 15:00:58 +0800

    第二次网站大体框架的搭建

---
+++ src/js/js/title.js
@@ -0,0 +1,5 @@
+$("#title_admin").load("title.html",function(){
+			layui.use('element', function(){
+			  var $ = layui.jquery
+			  ,element = layui.element; })
+		})	
---
+++ src/js/js/title_admin.js
@@ -0,0 +1,5 @@
+$("#title_admin").load("title_admin.html",function(){
+			layui.use('element', function(){
+			  var $ = layui.jquery
+			  ,element = layui.element; })
+		})	
